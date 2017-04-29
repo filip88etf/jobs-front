919fcdc1f029aa837b710914d0e426e4ebbb9a6b
Filip <filip88etf@gmail.com>
2017-04-29 18:42:50 +0200

    Refactor of Input Text

--- app/Shared/InputText/input-text.component.ts
+++ app/Shared/InputText/input-text.component.ts
@@ -1,13 +1,65 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { NG_VALIDATORS, NG_VALUE_ACCESSOR, ControlValueAccessor, FormControl } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   selector: 'app-input-text',
-  templateUrl: 'input-text.component.html'
+  templateUrl: 'input-text.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputTextComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => InputTextComponent),
+      multi: true
+    }
+  ]
 })
 
-export class InputTextComponent {
-  @Input() model: string;
+export class InputTextComponent implements ControlValueAccessor, OnInit {
   @Input() label: string;
   @Input() required: boolean = false;
+  @Input() minLength: number = 0;
+  private propagateChange = (_: any) => {};
+  touched: boolean = false;
+  valid: boolean = true;
+  model: any;
+  minLengthError: boolean = false;
+  requiredError: boolean = false;
+  ngOnInit() {
+  }
+
+  valueChanged() {
+    this.propagateChange(this.model);
+  }
+
+  validate(control: FormControl) {
+    let value = control.value;
+
+    if (this.touched && this.required) {
+      this.requiredError = !(value && value.length);
+    }
+    if (this.touched && this.minLength > 0 && value) {
+      this.minLengthError = value.length < this.minLength;
+    }
+    this.valid = !(this.minLengthError || this.requiredError);
+    this.touched = true;
+  }
+
+  public writeValue(obj: any) {
+    this.model = obj;
+    this.valid = true;
+    this.minLengthError = false;
+    this.requiredError = false;
+    this.touched = false;
+  }
+
+  public registerOnChange(fn: any) {
+      this.propagateChange = fn;
+  }
+
+  public registerOnTouched() { }
 }
