2adf5c1e82051e19ae43583b0f2398c89dda5f77
Filip <filip88etf@gmail.com>
2017-08-12 15:23:46 +0200

    edit job picture

--- app/User/Jobs/Edit/edit-job.component.ts
+++ app/User/Jobs/Edit/edit-job.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ImageCropperComponent, CropperSettings } from 'ng2-img-cropper';
 
 import { JobService } from '../../../Jobs/job.service';
 import { Helper } from '../../../helper';
@@ -11,7 +12,8 @@ import { Job } from '../../../Jobs/Job';
 @Component({
   moduleId: module.id,
   selector: 'app-edit-job',
-  templateUrl: 'edit-job.component.html'
+  templateUrl: 'edit-job.component.html',
+  styles: ['.image-cropper { padding: 20px 0px }']
 })
 
 export class EditJobComponent implements OnInit {
@@ -19,10 +21,17 @@ export class EditJobComponent implements OnInit {
   selected: Option[];
   regions: Option[] = CITIES;
   professions: Option[] = PROFESSIONS;
+  cropperSettings: CropperSettings;
+  data: Object;
   job: Job;
+  @ViewChild('cropper', undefined) cropper: ImageCropperComponent;
 
   constructor(public activeModal: NgbActiveModal, private jobService: JobService,
     private formBuilder: FormBuilder) {
+      this.cropperSettings = new CropperSettings();
+      this.cropperSettings.noFileInput = true;
+      this.cropperSettings.allowedFilesRegex = (/\.(gif|jpg|jpeg|tiff|png)$/i);
+      this.data = {};
   }
 
   ngOnInit() {
@@ -33,11 +42,11 @@ export class EditJobComponent implements OnInit {
     });
   }
 
-  init(job: Job) {
+  public init(job: Job) {
     this.job = job;
   }
 
-  submit(): void {
+  public submit(): void {
     let job: Job = new Job();
 
     job.id = this.job.id;
@@ -51,7 +60,22 @@ export class EditJobComponent implements OnInit {
     }
   }
 
-  close(): void {
+  public fileChange(event: any) {
+    let image: any = new Image(),
+        file: File = event.target.files[0],
+        myReader: FileReader = new FileReader();
+
+    myReader.onloadend = (loadEvent: any) => {
+        image.src = loadEvent.target.result;
+        this.job.imageURL = image.src;
+        this.cropper.setImage(image);
+    };
+
+    myReader.readAsDataURL(file);
+  }
+
+
+  public close(): void {
     this.activeModal.dismiss('close');
   }
 }
