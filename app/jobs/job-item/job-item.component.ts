2b233e67e3f6a901dbb06bbc0c0c060a4ac3a3a9
Filip <filip88etf@gmail.com>
2017-09-12 00:37:16 +0200

    apply for job on list of jobs

--- app/jobs/job-item/job-item.component.ts
+++ app/jobs/job-item/job-item.component.ts
@@ -1,8 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+import { ApplyModalComponent } from '../apply-modal/apply-modal.component';
 import { JobService } from '../job.service';
 import { Job } from '../Job';
+import { User } from '../../user/User';
+import { Application } from '../../applications/Application';
 
 @Component({
   moduleId: module.id,
@@ -11,16 +15,50 @@ import { Job } from '../Job';
   styleUrls: ['job-item.component.css']
 })
 
-export class JobItemComponent {
+export class JobItemComponent implements OnInit {
   @Input() job: Job;
+  @Input() applications: Application[];
+  @Input() loggedUser: User;
+  isApplied = false;
+  isQualified = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private modalService: NgbModal) {
   }
 
-  public openJobDetails() {
+  ngOnInit() {
+    this.checkCanApply();
+  }
+
+  public openJobDetails(): void {
     this.router.navigate(['jobs/details', { id: this.job.id, username: this.job.username }]);
   }
 
-  public report() {
+  public apply (): void {
+    let modal = this.modalService.open(ApplyModalComponent);
+
+    modal.componentInstance.init(this.job, this.loggedUser);
+    modal.result.then(
+      (result) => {
+        this.isApplied = true;
+        this.applications.push(result);
+      },
+      (reason) => { }
+    );
+  }
+
+  private checkCanApply(): void {
+    if (this.applications) {
+      for (let i = 0; i < this.applications.length; i++) {
+        if (this.applications[i]['jobId'] === this.job.id) {
+          this.isApplied = true;
+        }
+      }
+    }
+    if (this.loggedUser.type === 'worker' && this.job.profession === this.loggedUser['profession']) {
+      this.isQualified = true;
+    }
+  }
+
+  public report(): void {
   }
 }
--- app/jobs/list/jobs-list.component.ts
+++ app/jobs/list/jobs-list.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 
 import { Job } from '../Job';
+import { User } from '../../user/User';
+import { Application } from '../../applications/Application';
 import { JobService } from '../job.service';
+import { ApplicationService } from '../../applications/application.service';
+import { UserService } from '../../user/user.service';
 
 @Component({
   moduleId: module.id,
@@ -17,17 +22,29 @@ export class JobsListComponent {
   jobs: Job[];
   filter: any;
   size: number = 10;
+  applications: Application[];
+  loggedUser: boolean;
 
-  constructor(private router: Router, private route: ActivatedRoute, private jobService: JobService) {
+  constructor(private router: Router, private route: ActivatedRoute, private jobService: JobService,
+    private applicationService: ApplicationService, private userService: UserService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.filter = Object.assign({}, params);
-      this.jobService.list(this.filter).subscribe(
-        (response: any) => {
-          this.jobs = response.content;
-          this.totalNumber = response.page.totalElements;
+      this.userService.isLogged().subscribe(
+        (user: any) => {
+          this.loggedUser = user;
+          if (user && user.type === 'worker') {
+            this.mapJobsAndWorkerApplications(user);
+          } else {
+            this.jobService.list(this.filter).subscribe(
+              (response: any) => {
+                this.jobs = response.content;
+                this.totalNumber = response.page.totalElements;
+              }
+            );
+          }
         }
       );
     });
@@ -36,5 +53,16 @@ export class JobsListComponent {
   public pageChanged(pageNumber: number) {
     this.filter.page = this.page = pageNumber;
     this.router.navigate(['jobs', this.filter]);
+  }
+
+  private mapJobsAndWorkerApplications (user: User) {
+    forkJoin([
+      this.jobService.list(this.filter),
+      this.applicationService.getByWorkerId(user.id)
+    ]).subscribe((results: any) => {
+      this.jobs = results[0].content;
+      this.applications = results[1];
+      this.totalNumber = results[0].page.totalElements;
+    });
   }
 }
